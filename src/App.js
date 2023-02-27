ee1a6be299ac291c36cf01cbb2d3b60ad3f570db
Ivaylo <ignatov.ivaylo@gmail.com>
2023-02-27 10:45:31 +0200

    add container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Container from './Components/Container/Container';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        
+      </Container>
     </div>
   );
 }
