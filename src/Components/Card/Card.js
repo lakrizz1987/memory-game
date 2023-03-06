7598386cd1f7f9c34d7cc8ffec9d5156e6f0b1af
Ivaylo <ignatov.ivaylo@gmail.com>
2023-03-06 10:26:03 +0200

    change img path

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,7 +4,7 @@ const Card = ({ item, handleClick,id }) => {
         const itemClass = item.stat ? ' active ' + item.stat : ''
     return (
         <div className={'cardBox' + itemClass}  onClick={() => handleClick(id)}>
-            <img className='imageFootboler'  src={`memory-game/images/${item.img}`} alt='logo' />
+            <img className='imageFootboler'  src={`/memory-game/images/${item.img}`} alt='logo' />
         </div>
     )
 }
